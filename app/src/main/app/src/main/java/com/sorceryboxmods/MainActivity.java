479c23d04e5019d62f748af0562705fcc482971b
corbinkean04-lang <corbinkean04@gmail.com>
2026-09-12 19:18:29 -0700

    Create MainActivity.java

---
+++ app/src/main/app/src/main/java/com/sorceryboxmods/MainActivity.java
@@ -0,0 +1,39 @@
+package com.sorceryboxmods;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+    private static final String SITE = "https://07d9069e-fb15-4238-a990-fef0f93f599e.lovableproject.com/";
+
+    @Override protected void onCreate(Bundle state) {
+        super.onCreate(state);
+        webView = new WebView(this);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        if (state == null) webView.loadUrl(SITE); else webView.restoreState(state);
+        setContentView(webView);
+    }
+
+    @Override public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+
+    @Override protected void onSaveInstanceState(Bundle out) {
+        webView.saveState(out);
+        super.onSaveInstanceState(out);
+    }
+}
